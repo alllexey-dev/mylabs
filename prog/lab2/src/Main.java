81c8df13fd9b981c2fdc5d8921f0e02d3d8e81a6
alllexey <alllexey.dev@gmail.com>
2025-11-10 10:25:11 +0300

    upd

--- prog/lab2/src/Main.java
+++ prog/lab2/src/Main.java
@@ -10,10 +10,14 @@ public final class Main {
         Pokemon p2 = new Nidoking("alex", 40);
         Pokemon p3 = new Regigigas("rick", 40);
         Pokemon p4 = new Nidorino("mickey", 40);
+        Pokemon p5 = new NidoranM("igor", 40);
+        Pokemon p6 = new Lickitung("ivan", 40);
         b.addAlly(p1);
         b.addAlly(p2);
+        b.addAlly(p5);
         b.addFoe(p3);
         b.addFoe(p4);
+        b.addFoe(p6);
         b.go();
     }
 }
